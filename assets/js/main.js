847e015ace1c8043ac103adeb322c2d32e0a89d0
Łukasz Kałużny <lukasz@kaluzny.pro>
2020-02-09 20:57:21 +0100

    clean up

--- assets/js/main.js
+++ assets/js/main.js
@@ -122,84 +122,6 @@ jQuery(document).ready(function ($) {
 	};
 	// siteSliderRange();
 
-
-	var siteMagnificPopup = function () {
-		$('.image-popup').magnificPopup({
-			type: 'image',
-			closeOnContentClick: true,
-			closeBtnInside: false,
-			fixedContentPos: true,
-			mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
-			gallery: {
-				enabled: true,
-				navigateByImgClick: true,
-				preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-			},
-			image: {
-				verticalFit: true
-			},
-			zoom: {
-				enabled: true,
-				duration: 300 // don't foget to change the duration also in CSS
-			}
-		});
-
-		$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-			disableOn: 700,
-			type: 'iframe',
-			mainClass: 'mfp-fade',
-			removalDelay: 160,
-			preloader: false,
-
-			fixedContentPos: false
-		});
-	};
-	siteMagnificPopup();
-
-
-	var siteCarousel = function () {
-		if ($('.nonloop-block-13').length > 0) {
-			$('.nonloop-block-13').owlCarousel({
-				center: false,
-				items: 1,
-				loop: false,
-				stagePadding: 0,
-				margin: 20,
-				nav: true,
-				navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
-				responsive: {
-					600: {
-						margin: 20,
-						items: 2
-					},
-					1000: {
-						margin: 20,
-						stagePadding: 0,
-						items: 2
-					},
-					1200: {
-						margin: 20,
-						stagePadding: 0,
-						items: 3
-					}
-				}
-			});
-		}
-
-		$('.slide-one-item').owlCarousel({
-			center: false,
-			items: 1,
-			loop: true,
-			stagePadding: 0,
-			margin: 0,
-			autoplay: true,
-			pauseOnHover: false,
-			nav: true,
-			navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">']
-		});
-	};
-	siteCarousel();
-
 	var siteStellar = function () {
 		$(window).stellar({
 			responsive: false,
@@ -211,19 +133,5 @@ jQuery(document).ready(function ($) {
 		});
 	};
 	siteStellar();
-
-	var siteCountDown = function () {
-
-		$('#date-countdown').countdown('2020/10/10', function (event) {
-			var $this = $(this).html(event.strftime(''
-				+ '<span class="countdown-block"><span class="label">%w</span> weeks </span>'
-				+ '<span class="countdown-block"><span class="label">%d</span> days </span>'
-				+ '<span class="countdown-block"><span class="label">%H</span> hr </span>'
-				+ '<span class="countdown-block"><span class="label">%M</span> min </span>'
-				+ '<span class="countdown-block"><span class="label">%S</span> sec</span>'));
-		});
-
-	};
-	siteCountDown();
 
 });
