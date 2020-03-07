6ab636698f5c950753a54f17db26b5a3b595bc07
Łukasz Kałużny <lukasz@kaluzny.pro>
2020-03-07 21:55:09 +0100

    zmiana obrazu w docker

--- docker.sh
+++ docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker rm -f pato
-docker run --rm -it -p 4000:4000 -v $(PWD):/site --name pato andredumas/github-pages serve --watch --force_polling
+docker run --rm -it -p 4000:4000 -v $(PWD):/srv/jekyll --name pato jekyll/jekyll:3.8.5 jekyll serve --drafts --watch --force_polling
