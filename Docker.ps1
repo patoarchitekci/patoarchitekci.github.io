fafce2ee3d18d38714a18ee068f951e15b1d995d
Łukasz Kałużny <lukasz@kaluzny.pro>
2023-12-08 09:08:27 +0000

    Fix devcontainer

--- Docker.ps1
+++
@@ -1,2 +0,0 @@
-docker rm -f pato
-docker run --rm -it -p 4000:4000 -v //c/src/patoarchitekci.github.io/:/site --name pato andredumas/github-pages serve --watch --force_polling
--- docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker rm -f pato
-docker run --rm -it -p 4000:4000 -v $(PWD):/srv/jekyll --name pato jekyll/jekyll:3.8.5 jekyll serve --drafts --watch
